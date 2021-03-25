79d429c0df4451c026f7e09099ab258b32ed1c9d
yeunk3000 <75449002+yeunk3000@users.noreply.github.com>
2021-03-25 13:44:28 -0400

    Add files via upload

---
+++ exercise1.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "
+Hello world!
+Hello world!
+Hello world!
+Hello world!
+Hello world!
+Hello world!
+Hello world!
+Hello world!
+Hello world!
+Hello world!"
+
+sleep 2m
+
+ls -a ~ | wc -l > exercise1.txt
